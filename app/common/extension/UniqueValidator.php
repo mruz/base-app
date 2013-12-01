3b3c6a15b238af6e113ee256eaa21b07d7ab5eea
mruz <mruz@poczta.onet.pl>
2013-12-01 16:00:43 +0100

    Users signup fix

---
+++ app/common/extension/UniqueValidator.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Unique Validator
+ * 
+ * @package     base-app
+ * @category    Extension
+ * @version     1.2
+ */
+
+namespace Baseapp\Extension;
+
+class UniqueValidator extends \Phalcon\Validation\Validator implements \Phalcon\Validation\ValidatorInterface
+{
+
+    public function validate($validator, $attribute)
+    {
+        if (!$this->isSetOption('model'))
+            return FALSE;
+
+        $model = '\Baseapp\Models\\' . ucfirst($this->getOption('model'));
+        $value = $validator->getValue($attribute);
+
+        $filtered = $model::findFirst(array($attribute . '=:atribute:', 'bind' => array('atribute' => $value)));
+
+
+        if ($filtered) {
+
+            $message = $this->getOption('message');
+            if (!$message) {
+                $message = __(':field must be unique', array(':field' => ucfirst($attribute)));
+            }
+
+            $validator->appendMessage(new \Phalcon\Validation\Message($message, $attribute, 'Unique'));
+
+            return false;
+        }
+
+        return true;
+    }
+
+}
--- app/common/models/Users.php
+++ app/common/models/Users.php
@@ -10,10 +10,21 @@
 
 namespace Baseapp\Models;
 
-use Baseapp\Library\Auth;
+use Baseapp\Library\Auth,
+    Baseapp\Library\Email;
 
 class Users extends \Phalcon\Mvc\Model
 {
+
+    public function validation()
+    {
+        $this->validate(new \Phalcon\Mvc\Model\Validator\Uniqueness(array(
+            'field' => 'username'
+        )));
+        if ($this->validationHasFailed() == true) {
+            return false;
+        }
+    }
 
     /**
      * User initialize
@@ -22,18 +33,18 @@ class Users extends \Phalcon\Mvc\Model
      */
     public function initialize()
     {
-        $this->hasMany("id", __NAMESPACE__."\Tokens", "user_id", array(
+        $this->hasMany("id", __NAMESPACE__ . "\Tokens", "user_id", array(
             'foreignKey' => array(
                 'acrion' => \Phalcon\Mvc\Model\Relation::ACTION_CASCADE
             )
         ));
-        $this->hasMany("id", __NAMESPACE__."\RolesUsers", "user_id", array(
+        $this->hasMany("id", __NAMESPACE__ . "\RolesUsers", "user_id", array(
             'foreignKey' => array(
                 'acrion' => \Phalcon\Mvc\Model\Relation::ACTION_CASCADE
             )
         ));
     }
-    
+
     /**
      * Sign up User method
      *
@@ -42,46 +53,48 @@ class Users extends \Phalcon\Mvc\Model
     public function signup()
     {
         $validation = new \Phalcon\Validation();
-        
+
         $validation->add('username', new \Phalcon\Validation\Validator\PresenceOf());
+        $validation->add('username', new \Baseapp\Extension\UniqueValidator(array(
+            'model' => 'Users',
+        )));
         $validation->add('username', new \Phalcon\Validation\Validator\StringLength(array(
             'min' => 4,
         )));
-        
+
         $validation->add('password', new \Phalcon\Validation\Validator\PresenceOf());
         $validation->add('repeatPassword', new \Phalcon\Validation\Validator\Confirmation(array(
             'with' => 'password'
         )));
-        
+
         $validation->add('email', new \Phalcon\Validation\Validator\PresenceOf());
         $validation->add('email', new \Phalcon\Validation\Validator\Email());
-        
+        $validation->add('email', new \Baseapp\Extension\UniqueValidator(array(
+            'model' => 'Users',
+        )));
+
         $validation->add('repeatEmail', new \Phalcon\Validation\Validator\Confirmation(array(
             'with' => 'email'
         )));
 
         $messages = $validation->validate($_POST);
-        
-        if(count($messages))
-        {
+
+        if (count($messages)) {
             return $validation->getMessages();
-        }
-        else
-        {
-            $date = date('Y-m-d H:i:s');
+        } else {
             $user = new Users();
             $user->username = $this->request->getPost('username');
             $user->password = Auth::instance()->hash($this->request->getPost('password'));
             $user->email = $this->request->getPost('email');
-            $user->date = $date;
             $user->logins = 0;
             $user->create();
-            
-            $hash = $user->hash = md5($user->id.$user->email.$date.$this->config->auth->hash_key);
-            $user->update();
-            
-            // send e-mail with url to confirm
-            
+
+            $hash = md5($user->id . $user->email . $user->password . $this->config->auth->hash_key);
+
+            $email = new Email();
+            $email->prepare(__('Activation'), $this->request->getPost('email'), 'activation', array('username' => $this->request->getPost('username'), 'hash' => $hash));
+            $email->Send();
+
             unset($_POST);
             return TRUE;
         }
