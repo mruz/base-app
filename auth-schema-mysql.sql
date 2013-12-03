c73ac5eceee0b4b4cc4e7f4101a0fed67adce17a
web777 <web@biz.by>
2013-12-03 22:10:03 +0700

    added default admin:admin user

--- auth-schema-mysql.sql
+++ auth-schema-mysql.sql
@@ -1,49 +1,92 @@
-CREATE TABLE IF NOT EXISTS `roles` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+﻿
+#
+# Structure for the `roles` table : 
+#
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
   `description` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-INSERT INTO `roles` (`id`, `name`, `description`) VALUES(1, 'login', 'Login privileges, granted after account confirmation');
-INSERT INTO `roles` (`id`, `name`, `description`) VALUES(2, 'admin', 'Administrative user, has access to everything.');
+#
+# Structure for the `users` table : 
+#
 
-CREATE TABLE IF NOT EXISTS `roles_users` (
-  `user_id` int(10) UNSIGNED NOT NULL,
-  `role_id` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY  (`user_id`,`role_id`),
-  KEY `fk_role_id` (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(254) NOT NULL,
   `username` varchar(32) NOT NULL DEFAULT '',
   `password` varchar(64) NOT NULL,
-  `logins` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `last_login` int(10) UNSIGNED,
-  PRIMARY KEY  (`id`),
+  `logins` int(10) unsigned NOT NULL DEFAULT '0',
+  `last_login` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_username` (`username`),
   UNIQUE KEY `uniq_email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `user_tokens` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) UNSIGNED NOT NULL,
+#
+# Structure for the `roles_users` table : 
+#
+
+DROP TABLE IF EXISTS `roles_users`;
+
+CREATE TABLE `roles_users` (
+  `user_id` int(10) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `fk_role_id` (`role_id`),
+  CONSTRAINT `roles_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `roles_users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `user_tokens` table : 
+#
+
+DROP TABLE IF EXISTS `user_tokens`;
+
+CREATE TABLE `user_tokens` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
   `user_agent` varchar(40) NOT NULL,
   `token` varchar(40) NOT NULL,
-  `created` int(10) UNSIGNED NOT NULL,
-  `expires` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY  (`id`),
+  `created` int(10) unsigned NOT NULL,
+  `expires` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_token` (`token`),
   KEY `fk_user_id` (`user_id`),
-  KEY `expires` (`expires`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  KEY `expires` (`expires`),
+  CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `roles_users`
-  ADD CONSTRAINT `roles_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `roles_users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+#
+# Data for the `roles` table  (LIMIT 0,500)
+#
 
-ALTER TABLE `user_tokens`
-  ADD CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+INSERT INTO `roles` (`id`, `name`, `description`) VALUES 
+  (1,'login','Login privileges, granted after account confirmation'),
+  (2,'admin','Administrative user, has access to everything.');
+COMMIT;
+
+#
+# Data for the `users` table  (LIMIT 0,500)
+#
+
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `logins`, `last_login`) VALUES 
+  (1,'admin@admin.com','admin','b6d1498a9911c91cab6becf1809787cf6efa06cf88390d78433090196617907f',4,1386082850);
+COMMIT;
+
+#
+# Data for the `roles_users` table  (LIMIT 0,500)
+#
+
+INSERT INTO `roles_users` (`user_id`, `role_id`) VALUES 
+  (1,2);
+COMMIT;
