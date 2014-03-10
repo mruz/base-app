94aff18901881f20ce5e4c1778d9d66796855370
mruz <mruz@poczta.onet.pl>
2014-03-10 12:30:46 +0100

    Main task, displays available tasks

--- app/cli/tasks/MainTask.php
+++ app/cli/tasks/MainTask.php
@@ -24,14 +24,28 @@ class MainTask extends \Phalcon\CLI\Task
     }
 
     /**
-     * Main Action
+     * Main Action - display available tasks
      *
      * @package     base-app
      * @version     2.0
      */
     public function mainAction()
     {
-        echo "mainTask/mainAction\n";
+        echo "-- CLI tasks --\n";
+        foreach (new \DirectoryIterator(ROOT_PATH . '/app/cli/tasks') as $file) {
+            if ($file->isDot() || $file->getBasename('.php') == 'MainTask') {
+                continue;
+            }
+            $task = $file->getBasename('.php');
+            echo strtolower(strstr($task, 'Task', TRUE)) . "\n";
+
+            $f = new \ReflectionClass(__NAMESPACE__ . '\\' . $task);
+            foreach ($f->getMethods() as $m) {
+                if ($m->class == __NAMESPACE__ . '\\' . $task && $m->name != 'initialize') {
+                    echo "\t" . strstr($m->name, 'Action', TRUE) . "\n";
+                }
+            }
+        }
     }
 
     /**
